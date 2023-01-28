44f88292965898bd67964f961fca2d539e25772a
Pollytime <yoropolytime@gmail.com>
2023-01-27 18:12:51 -0800

    Please for us

--- main.h
+++ main.h
@@ -21,16 +21,21 @@
 
 /**
  * struct parameters - parameters struct
+ *
  * @unsign: flag if unsigned value
+ *
  * @plus_flag: on if plus_flag specified
  * @space_flag: on if hashtag_flag specified
  * @hashtag_flag: on if _flag specified
  * @zero_flag: on if _flag specified
  * @minus_flag: on if _flag specified
+ *
  * @width: field width specified
  * @precision: field precision specified
+ *
  * @h_modifier: on if h_modifier is specified
  * @l_modifier: on if l_modifier is specified
+ *
  */
 
 typedef struct parameters
@@ -52,6 +57,7 @@ typedef struct parameters
 
 /**
  * struct specifier - Struct token
+ *
  * @specifier: format token
  * @f: The function associated
  */
@@ -108,6 +114,7 @@ void init_params(params_t *params, va_list ap);
 
 /* string_fields.c modoule */
 char *get_precision(char *p, params_t *params, va_list ap);
+
 /* _prinf.c module */
 int _printf(const char *format, ...);
 
